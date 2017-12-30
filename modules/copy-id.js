b5b1641acfbadde2d39157a78166b117067f852c
Dominic Tarr <dominic.tarr@gmail.com>
2017-12-30 22:19:30 +1300

    copy-id for both messages and feeds

--- modules/copy-id.js
+++ modules/copy-id.js
@@ -1,23 +1,43 @@
 var isElectron = require('is-electron')
 
 exports.gives = {
-  message: { action: true }
+  message: { action: true },
+  avatar: { action: true }
 }
 
 exports.create = function (api) {
-  return { message: { action: function (data) {
-    if(isElectron()) {
-      var a = document.createElement('a')
-      a.href = '#'
-      a.textContent = 'copy id'
-      a.title = 'click to copy this message id to the clipboard'
-      a.onclick = function () {
-        require('electron').clipboard.writeText(data.key)
+  return {
+    message: { action: function (data) {
+      if(isElectron()) {
+        var a = document.createElement('a')
+        a.href = '#'
+        a.textContent = 'copy id'
+        a.title = 'click to copy this message id to the clipboard'
+        a.onclick = function () {
+          require('electron').clipboard.writeText(data.key)
+        }
+        return a
       }
-      return a
     }
-  }}}
+  },
+  avatar: { action: function (id) {
+      if(isElectron()) {
+        var a = document.createElement('a')
+        a.href = '#'
+        a.textContent = 'copy id'
+        a.title = 'click to copy this message id to the clipboard'
+        a.onclick = function () {
+          require('electron').clipboard.writeText(id)
+        }
+        return a
+      }
+    }
+  },
+
+
+  }
 }
+
 
 
 

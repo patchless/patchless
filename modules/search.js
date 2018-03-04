dc12e47b05793970c33353d7f2a942e53528fbb2
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-04 22:37:52 +1300

    search field on the viewMenu

---
+++ modules/search.js
@@ -0,0 +1,41 @@
+'use strict'
+var h = require('hyperscript')
+var SuggestBox = require('suggest-box')
+
+exports.needs= {
+  suggest: { search: 'first' },
+  nav: {goto: 'first'}
+}
+
+exports.gives = {
+  app: { viewMenu: true }
+}
+
+exports.create = function (api) {
+
+  //okay this shouldn't be in the patchless repo,
+  //should be somewhere else...
+
+  return { app: { viewMenu: function (src) {
+    var input = h('input')
+    var wrapped = h('span', input)
+
+    SuggestBox(input, function (word, cb) {
+      var fn = api.suggest.search(word)
+      if(!fn) return cb()
+      fn(word, cb)
+    })
+
+    input.onkeydown = function (ev) {
+
+      if(ev.keyCode !== 13) return
+      if(api.nav.goto(input.value))
+        input.value = ''
+    }
+
+    return wrapped
+  }}}
+
+}
+
+
